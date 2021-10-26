824ec8f4c38bf75f97f863a945a5a863d997f1e9
mmsesay <maej_01@hotmail.com>
2021-10-26 17:35:56 +0000

    Install react-router-dom Add: header with navigations

---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+export const Home = () => (
+  <div className="bg-white w-2/6 h-2/4 rounded oveflow-hidden">
+    <h1>Home.js</h1>
+  </div>
+); 
---
+++ src/components/Quote.js
@@ -0,0 +1,5 @@
+export const Quote = () => (
+  <div className="bg-white w-2/6 h-2/4 rounded oveflow-hidden">
+    <h1>Quote.js</h1>
+  </div>
+); 
