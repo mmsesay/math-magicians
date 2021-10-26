a68bea449bddeab688557f89fe6d9649daea6bef
mmsesay <maej_01@hotmail.com>
2021-10-26 17:52:35 +0000

    Add: content to the home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 export const Home = () => (
-  <div className="bg-white w-2/6 h-2/4 rounded oveflow-hidden">
-    <h1>Home.js</h1>
+  <div className="w-full h-auto my-24 py-10 px-20 space-y-10 text-white text-justify">
+    <h1 className="text-4xl font-bold mb-20">Welcome to our page!</h1>
+    <p className="text-2xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices, 
+      purus sit amet lobortis egestas, libero enim rhoncus libero, in posuere dui velit at libero. 
+      Sed non tellus tortor. Maecenas euismod tortor ex, eu placerat odio egestas quis. 
+      Proin dignissim elit quam, nec facilisis quam laoreet a. Donec in dignissim erat, 
+      sit amet elementum lectus. Donec maximus est vulputate, aliquet eros nec, finibus velit. 
+      Nulla at lacus accumsan, euismod diam ac, pretium libero.
+    </p>
+    <p className="text-2xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices, 
+      purus sit amet lobortis egestas, libero enim rhoncus libero, in posuere dui velit at libero. 
+      Sed non tellus tortor. Maecenas euismod tortor ex, eu placerat odio egestas quis. 
+      Proin dignissim elit quam, nec facilisis quam laoreet a. Donec in dignissim erat, 
+      sit amet elementum lectus. Donec maximus est vulputate, aliquet eros nec, finibus velit. 
+      Nulla at lacus accumsan, euismod diam ac, pretium libero.
+    </p>
   </div>
 ); 
