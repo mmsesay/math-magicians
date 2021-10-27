383dea5d17e89eebbd1539ee278690edfc7ad681
mmsesay <maej_01@hotmail.com>
2021-10-27 22:46:16 +0000

    Add: test for calculator snapshot

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { Calculator } from '../components';
 
 describe('Tests calculator components', () => {
@@ -18,5 +19,10 @@ describe('Tests calculator components', () => {
     render(<Calculator />);
     const numbersButtons = screen.queryAllByTestId('keypad-button');
     expect(numbersButtons).toHaveLength(19);
+  });
+
+  test('renders as expected', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
