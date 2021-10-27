2a76ce1bc6add8f908b78ca962622fdce31c22b1
Rayan84 <rayan_84@live.com>
2021-10-27 23:52:45 +0200

    Add test logic for paragraphs in the homepage

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import { Home } from '../components/Home';
+import { Home } from '../components';
 
 describe('Testing homepage', () => {
   test('Tests the existence of the the title: Welcome to our page!', () => {
     render(<Home />);
     const h1 = screen.getByText(/Welcome to our page!/);
     expect(h1).toBeInTheDocument();
+  });
+
+  test('Tests the existence of the first paragraph', () => {
+    render(<Home />);
+    const paragraphs = screen.queryAllByTestId('paragraph');
+    expect(paragraphs).toHaveLength(2);
   });
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 export const Home = () => (
   <div className="w-full h-auto my-24 py-10 px-20 space-y-10 text-justify">
     <h1 className="text-4xl text-gray-300 font-bold mb-20">Welcome to our page!</h1>
-    <p className="text-2xl text-gray-200">
+    <p data-testid="paragraph" className="text-2xl text-gray-200">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices, 
       purus sit amet lobortis egestas, libero enim rhoncus libero, in posuere dui velit at libero. 
       Sed non tellus tortor. Maecenas euismod tortor ex, eu placerat odio egestas quis. 
@@ -9,7 +9,7 @@ export const Home = () => (
       sit amet elementum lectus. Donec maximus est vulputate, aliquet eros nec, finibus velit. 
       Nulla at lacus accumsan, euismod diam ac, pretium libero.
     </p>
-    <p className="text-2xl text-gray-200">
+    <p data-testid="paragraph" className="text-2xl text-gray-200">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices, 
       purus sit amet lobortis egestas, libero enim rhoncus libero, in posuere dui velit at libero. 
       Sed non tellus tortor. Maecenas euismod tortor ex, eu placerat odio egestas quis. 
